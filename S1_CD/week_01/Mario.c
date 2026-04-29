bf738f63b75fd0f25987a035aa95d2b0d35fe04c
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-04-29 10:20:15 +0330

    Update Mario.c

--- S1_CD/week_01/Mario.c
+++ S1_CD/week_01/Mario.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
 {
+
     //First we need to get the grid scale from the user
     do
     {
@@ -11,9 +12,8 @@ int main(void){
       // and i did it because we need a number from 1 to 8
       while (scale < 1 || scale > 8);
       // now we need to write a code which makes the grid we want (IDK how to do this rn but i can write a function and write my codes into it later)
-     void Print_Mario(int scl)
+}
+ void Print_Mario(int scl)
         {
             //we want to write our codes here
-        
         }
-}
