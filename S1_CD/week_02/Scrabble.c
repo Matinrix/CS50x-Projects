c307ec5a05ec70185462a49a8a1cf1854aa7cdc7
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-09 14:05:39 +0330

    Some bugs got fixed

--- S1_CD/week_02/Scrabble.c
+++ S1_CD/week_02/Scrabble.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 // points:
-int POINTS[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
-char Letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int POINTS[26] = {1, 3, 3, 2, 1, 4,
+2, 4, 1, 8,
+5, 1, 3, 1, 1,
+3, 10, 1, 1, 1,
+1, 4, 4, 8, 4, 10};
+char Letters[26] = {'A', 'B', 'C', 'D', 
+'E', 'F', 'G', 'H',
+'I', 'J', 'K', 'L', 'M',
+'N', 'O', 'P', 'Q', 'R', 'S',
+'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 int compute_score(char *word);
 int GetLen(char *word);
-char toUpper(char *word);
+char toUpper(char word);
 
 int main(void)
 {
     // get input words from both players
-    char word1[];
-    char word2[];
-    printf("Player 1 ; ");
+    char word1[255];
+    char word2[255];
+    printf("Player 1 : ");
     scanf("%s", &word1);
-    printf("Player 2; ");
+    printf("Player 2 : ");
     scanf("%s", &word2);
     // score botch words
     int score1 = compute_score(word1);
@@ -51,7 +59,7 @@ int compute_score(char *word)
     }
     return score;
 }
-char toUpper(char *word)
+char toUpper(char word)
 {
     if (word >= 'a' && word <= 'z')
     {
