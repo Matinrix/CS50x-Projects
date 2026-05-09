f0a1610f89ddd7e91ac8b0e51110f86d5adb9f05
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-09 14:42:02 +0330

    Code improved!
    i have fixed some bugs and also the code got better and shorter than past

--- S1_CD/week_02/Scrabble2.c
+++ S1_CD/week_02/Scrabble2.c
@@ -1,55 +1,31 @@
 #include <stdio.h>
-int POINTS[26] = {1, 3, 3, 2,
-1, 4, 2, 4
-, 1, 8, 5, 1
-, 3, 1, 1, 3
-, 10, 1, 1, 1
-, 1, 4, 4, 8,
- 4, 10};
-int getLen(char *word)
-{
-    int len = 0;
-    for (int i = 0; word[i] != '\0'; i++)
-    {
-        len++;
-    }
-    return len;
-}
-char toUpper(char *word)
-{
-    int Clen = getLen(word);
+#include <string.h>
+#include <ctype.h>
+int POINTS[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-    for (int i = 0; i < Clen; i++)
-    {
-        char cc = word[i];
-        if (cc <= 'z' && cc >= 'a')
-        {
-            return cc - ('A' - 'a');
-        }
-        return cc;
-    }
-}
-int compute_score(char *word)
+int compute_score(char *wordC)
 {
     int score = 0;
-    int len = getLen(word);
+    int len = strlen(wordC);
     for (int i = 0; i < len; i++)
     {
-        char cc = toUpper(word[i]);
-        int count = cc - 65;
-        score += POINTS[count];
+        char cc = toupper(wordC[i]);
+        if (cc >= 'A' && cc <= 'Z')
+        {
+            int count = cc - 'A';
+            score += POINTS[count];
+        }
     }
     return score;
 }
-
 int main()
 {
-    char *word1;
-    char *word2;
+    char word1[255];
+    char word2[255];
     printf("Player 1: ");
-    scanf("%s", &word1);
+    scanf("%s", word1);
     printf("Player 2: ");
-    scanf("%s", &word2);
+    scanf("%s", word2);
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
     if (score1 > score2)
@@ -60,9 +36,11 @@ int main()
     else if (score2 > score1)
     {
         printf("Player 2 wins!");
+        return 0;
     }
     else
     {
         printf("Tie!");
+        return 0;
     }
 }
