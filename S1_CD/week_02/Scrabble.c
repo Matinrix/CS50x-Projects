b025d374c8f924b48e3b4ae7878597c34586879d
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-09 14:03:56 +0330

    Update Scrabble.c

--- S1_CD/week_02/Scrabble.c
+++ S1_CD/week_02/Scrabble.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 // points:
-int POINTS[26] = {1, 3, 3, 2
-, 1, 4, 2, 4, 1, 8, 5, 1,
-3, 1, 1, 3, 10, 1, 1, 1,
-1, 4, 4, 8, 4, 10};
-char Letters[26] = {'A', 'B', 'C', 'D', 
-'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 
-'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 
-'U', 'V', 'W', 'X', 'Y', 'Z'};
+int POINTS[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+char Letters[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+int compute_score(char *word);
+int GetLen(char *word);
+char toUpper(char *word);
+
 int main(void)
 {
     // get input words from both players
-    char *word1[];
-    char *word2[];
+    char word1[];
+    char word2[];
     printf("Player 1 ; ");
     scanf("%s", &word1);
     printf("Player 2; ");
