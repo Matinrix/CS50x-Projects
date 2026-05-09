8437db34dc3465f2539875355f434a92e206f31a
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-09 13:55:50 +0330

    Update Scrabble2.c

--- S1_CD/week_02/Scrabble2.c
+++ S1_CD/week_02/Scrabble2.c
@@ -1,18 +1,63 @@
 #include <stdio.h>
-// points:
-int POINTS[26] = {1, 3, 3, 2, 1, 4, 2,
-4, 1, 8, 5, 1, 3, 1, 
-1, 3, 10, 1, 1, 1, 1,
-4, 4, 8, 4, 10};
-char Letters[26] = {'A', 'B', 'C', 'D', 'E',
-'F', 'G', 'H', 'I', 'J', 'K',
-'L', 'M', 'N', 'O', 'P', 'Q', 
-'R', 'S', 'T', 'U', 'V', 'W', 
-'X', 'Y', 'Z'};
-int compute_score(char *word);
-int GetLen(char *word);
-char toUpper(char *word);
-int main(void)
+int POINTS[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int getLen(char *word)
 {
- // CODE HERE
+    int len = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        len++;
+    }
+    return len;
+}
+char toUpper(char *word)
+{
+    int Clen = getLen(word);
+
+    for (int i = 0; i < Clen; i++)
+    {
+        char cc = word[i];
+        if (cc <= 'z' && cc >= 'a')
+        {
+            return cc - ('A' - 'a');
+        }
+        return cc;
+    }
+}
+int compute_score(char *word)
+{
+    int score = 0;
+    int len = getLen(word);
+    for (int i = 0; i < len; i++)
+    {
+        char cc = toUpper(word[i]);
+        int count = cc - 65;
+        score += POINTS[count];
+    }
+    return score;
+}
+
+int main()
+{
+    char *word1;
+    char *word2;
+    printf("Player 1: ");
+    scanf("%s", &word1);
+    printf("Player 2: ");
+    scanf("%s", &word2);
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+        return 0;
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
 }
