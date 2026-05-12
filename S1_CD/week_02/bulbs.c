383830e7df36d4e485687b4179acc901f30715cb
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-12 15:38:07 +0330

    Update bulbs.c

--- S1_CD/week_02/bulbs.c
+++ S1_CD/week_02/bulbs.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
+void print_bulb(int bit);
 int main (void)
 {
 
+}
+
+//You cannot change this function
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        printf("\U0001F7E1");
+    }
 }
