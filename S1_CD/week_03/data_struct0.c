c85b69dda79d7df69287c2610de5cd8a6a9dd4ed
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-13 18:53:51 +0330

    Create data_struct0.c

---
+++ S1_CD/week_03/data_struct0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    char *name;
+    char *Speed;
+    char *climb;
+    char *turnt;
+    char *weight;
+} jetData;
+int main(void)
+{
+
+}
