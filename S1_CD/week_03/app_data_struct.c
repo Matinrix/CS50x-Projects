155da32758b96e8fa1667d4d95c38406a3fe8d04
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-13 19:07:05 +0330

    Create jets data 🛦
    A simple C program that uses data structures and presents the user with a numbered list of fighter jet names, and the user can select one of them to view the specifications of the selected jet.💜

---
+++ S1_CD/week_03/app_data_struct.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+typedef struct
+{
+    char *name;
+    char *speed;
+    char *climb;
+    char *turnt;
+    char *weight;
+} jetData;
+int main(void)
+{
+
+}
