5b16f82d70ca0cb85b8de94c44cd9377cfc93f8d
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-13 18:52:00 +0330

    Update algorithm.c

--- S1_CD/week_03/algorithm.c
+++ S1_CD/week_03/algorithm.c
@@ -1,5 +1,18 @@
+/*this code is a simple code which ive just coded it
+    and its a sipmle linear algorithm whichis searching the arrays and tell us
+        if the users entered number exist in the array or not!
+*/
+int get_int(char *text);
 #include <stdio.h>
 int main (void)
 {
 
+}
+
+int get_int(char *text)
+{
+    int num;
+    printf("%s", text);
+    scanf("%i", &num);
+    return num;
 }
