de1244390be916724766a39b21c6c806260e2172
• Matin Derakhshan <85928677+Matinrix@users.noreply.github.com>
2026-05-14 20:26:26 +0330

    Initialize jet data for multiple aircraft

--- S1_CD/week_03/data_struct0.c
+++ S1_CD/week_03/data_struct0.c
@@ -9,7 +9,32 @@ typedef struct
     char *turnt;
     char *weight;
 } jetData;
+jetData jet[5];
 int main(void)
 {
-
+    jet[0].name = "F-15E";
+    jet[0].Speed = 2718;
+    jet[0].climb = 325;
+    jet[0].turnt = 21;
+    jet[0].weight = 15.33;
+    jet[1].name = "SU-30";
+    jet[1].Speed = 2304;
+    jet[1].climb = 240;
+    jet[1].turnt = 24;
+    jet[1].weight = 19.01;
+    jet[2].name = "F/A-18E";
+    jet[2].Speed = 1729;
+    jet[2].climb = 166.5;
+    jet[2].turnt = 24.4;
+    jet[2].weight = 8.9;
+    jet[3].name = "Rafaele";
+    jet[3].Speed = 1908;
+    jet[3].climb = 300;
+    jet[3].turnt = 20;
+    jet[3].weight = 9.42;
+    jet[4].name = "F-16";
+    jet[4].Speed = 2170;
+    jet[4].climb = 305;
+    jet[4].turnt = 19.5;
+    jet[4].weight = 9.08;
 }
